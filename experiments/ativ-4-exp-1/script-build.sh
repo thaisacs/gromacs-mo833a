af497c21ac5734f4c0e2ec53becc62e1c47b5f2e
thais <thaiscamachoo@gmail.com>
2020-04-19 12:21:13 -0300

    experiment final

--- experiments/ativ-4-exp-1/script-build.sh
+++ experiments/ativ-4-exp-1/script-build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #Author : Thais Camacho
 
+set OMP_NUM_THREADS=1
+
 DIR=~/Dev/gromacs-mo833a
 
 check_params() {
@@ -13,17 +15,22 @@ check_params() {
 check_params $#
 
 PARAM=$1
+echo $PARAM
 
 cd $DIR
 mkdir -p build
 cd build
 
 if [[ "$PARAM" == "Release" ]]; then
+  mkdir release
+  cd release
   echo "-- release mode"
-  cmake .. -DGMX_BUILD_OWN_FFTW=ON -DREGRESSIONTEST_DOWNLOAD=ON -DCMAKE_BUILD_TYPE=Release
+  cmake ../.. -DGMX_BUILD_OWN_FFTW=ON -DREGRESSIONTEST_DOWNLOAD=ON -DCMAKE_BUILD_TYPE=Release
 elif [[ "$PARAM" == "Debug" ]]; then
+  mkdir debug
+  cd debug
   echo "-- debug mode"
-  cmake .. -DGMX_BUILD_OWN_FFTW=ON -DREGRESSIONTEST_DOWNLOAD=ON -DCMAKE_BUILD_TYPE=Debug
+  cmake ../.. -DGMX_BUILD_OWN_FFTW=ON -DREGRESSIONTEST_DOWNLOAD=ON -DCMAKE_BUILD_TYPE=Debug
 else
   exit 0
 fi
