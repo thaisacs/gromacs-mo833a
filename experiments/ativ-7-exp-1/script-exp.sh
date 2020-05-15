ac0497510c2a4a49563faeaeaab404f224b6296a
thais <thaiscamachoo@gmail.com>
2020-05-15 18:43:12 -0300

    up

--- experiments/ativ-7-exp-1/script-exp.sh
+++ experiments/ativ-7-exp-1/script-exp.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 cd experiment
-mpirun -n 2 --hostfile ../hostfile2 ../../../build/bin/gmx_mpi mdrun -v -deffnm em 1> gmx.out 2> gmx.err
+mpirun -n 4 --hostfile ../hostfile ../../../build/bin/gmx_mpi mdrun -v -deffnm em 1> gmx.out 2> gmx.err
