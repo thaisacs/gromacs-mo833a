95c43f2c40fedf012e079ba3ad4738ef02614260
thais <thaiscamachoo@gmail.com>
2020-05-15 02:43:57 -0300

    script-build.sh without create build dic

--- experiments/ativ-7-exp-1/script-build.sh
+++ experiments/ativ-7-exp-1/script-build.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-cd ../..
-mkdir -p build
-cd build
+cd ../../build
 cmake .. -DGMX_BUILD_OWN_FFTW=ON -DREGRESSIONTEST_DOWNLOAD=ON -DCMAKE_C_COMPILER=mpicc -DCMAKE_CXX_COMPILER=mpicxx -DGMX_MPI=on
 make -j 3
